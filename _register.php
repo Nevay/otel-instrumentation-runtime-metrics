0b69b2e796756e669be49fec23abcb2ef43fcb7c
Tobias Bachert <git@b-privat.de>
2026-07-08 19:10:03 +0200

    Add missing `class_exists()` check in `_register.php`

--- _register.php
+++ _register.php
@@ -3,6 +3,11 @@ namespace Nevay\OTelInstrumentation\RuntimeMetrics;
 
 use Nevay\SPI\ServiceLoader;
 use OpenTelemetry\API\Instrumentation\AutoInstrumentation\Instrumentation;
+use function class_exists;
+
+if (!class_exists(ServiceLoader::class)) {
+    return;
+}
 
 ServiceLoader::register(Instrumentation::class, OpcacheMetrics::class);
 ServiceLoader::register(Instrumentation::class, ProcessMetrics::class);
